9453db7522538d12396a286bab6bd8adc999e483
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-11 22:38:46 +0530

    eleventh challenge for linked list

---
+++ delete-duplicate-value-nodes-from-a-sorted-linked-list.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the removeDuplicates function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function removeDuplicates(head) {
+    var node = new SinglyLinkedList;
+    var postnode = new SinglyLinkedList;
+    var prenode = new SinglyLinkedList;
+    node = head;
+    while (node != null) {
+        postnode = node.next;
+        prenode = node;
+        while (postnode!=null)
+        {
+            if (postnode.data == node.data) {
+                prenode.next = postnode.next;
+                postnode = postnode.next;
+            }
+            else {
+                prenode = postnode;
+                postnode = postnode.next;
+            }
+        }  
+        node = node.next;
+    }
+    return head;
+}
+function main() {
