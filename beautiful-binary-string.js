dcf66baf614e047df5f021c13533ef27ccf2a373
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-22 22:07:30 +0530

    Day 8 challenge 14

---
+++ beautiful-binary-string.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the beautifulBinaryString function below.
+function beautifulBinaryString(b) {
+    var len,count=0,last=0;
+    len = b.length;
+    for (var i = 0; i < len-2; i++)
+    {
+        if (b[i] == '0' && b[i + 1] == '1' && b[i + 2] == '0' && (i-last>2||i==0))
+        {
+            console.log(i);
+            last = i;
+            count++;
+        }
+    } 
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const b = readLine();
+
+    let result = beautifulBinaryString(b);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
