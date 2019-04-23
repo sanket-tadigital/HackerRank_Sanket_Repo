a23eabb61c47354e00a4e3db9335caa63c5c2b27
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-23 23:20:04 +0530

    Day 8 challenge 21

---
+++ string-construction.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the stringConstruction function below.
+function stringConstruction(s) {
+    var len = s.length;
+    var p = "",cost=0;
+    for (var i = 0; i < len; i++)
+    {
+        if (p.includes(s[i]))
+            p = p + s[i];
+        else {
+            cost++;
+            p = p + s[i];
+        }
+    }
+    return (cost);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = stringConstruction(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
